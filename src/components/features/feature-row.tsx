0cbb390188ab8589312c9cad761e19185329d646
manison88 <manison88@gmail.com>
2026-01-26 20:58:48 -0600

    Fix client directive syntax in feature-row.tsx

--- src/components/features/feature-row.tsx
+++ src/components/features/feature-row.tsx
@@ -1,4 +1,4 @@
-use client";
+"use client";
 
 import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
@@ -158,3 +158,4 @@ function TrashIcon({ className }: { className?: string }) {
       />
     </svg>
   );
+}
